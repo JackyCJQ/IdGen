c14dbf92a00deaaff290c4a47b3300e98e5bdd67
jacky_glo <jacky_glo@jacky.com>
2018-08-27 16:48:33 +0800

    first commit

--- IdService/src/main/java/com/jacky/service/impl/AbstractIdServiceImpl.java
+++ IdService/src/main/java/com/jacky/service/impl/AbstractIdServiceImpl.java
@@ -3,6 +3,8 @@ package com.jacky.service.impl;
 import com.jacky.service.bean.Id;
 import com.jacky.service.impl.bean.IdMeta;
 import com.jacky.service.impl.bean.IdType;
+import com.jacky.service.impl.converter.IdConverter;
+import com.jacky.service.impl.provider.MachineIdProvider;
 import com.jacky.service.interf.IdService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,7 +31,39 @@ public class AbstractIdServiceImpl implements IdService {
     protected IdType idType;
     protected IdMeta idMeta;
 
+    protected IdConverter idConverter;
+    protected MachineIdProvider machineIdProvider;
 
+    public AbstractIdServiceImpl() {
+        idType = IdType.MAX_PEAK;
+    }
+
+    public AbstractIdServiceImpl(String type) {
+        idType = IdType.parse(type);
+    }
+
+    public AbstractIdServiceImpl(IdType idType) {
+        this.idType = idType;
+    }
+
+    public void init() {
+        this.machineId = machineIdProvider.getMechineId();
+        if (machineId < 0) {
+            log.error("The machine ID is not configured properly so that Vesta Service refuses to start.");
+
+            throw new IllegalStateException(
+                    "The machine ID is not configured properly so that Vesta Service refuses to start.");
+
+        }
+
+
+
+    }
+
+
+    public IdMeta getIdMeta() {
+        return idMeta;
+    }
 
     public long genId() {
         return 0;
